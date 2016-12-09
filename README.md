ce36550a155c06246860685d3190419768e09997
geekpi <zhengsihua01@gmail.com>
2016-12-09 23:11:12 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,30 @@
+##How to use
+> 1. `pip3 install -r requirement.txt`
+> 2. configure your own adapter in `gfwlist.yaml` or `whitelist.yaml`
+> 3. `python3 spechtlite_conf.py`
+> 4. copy or link files which in `conf` to `~/SpechtLiteConf`
+
+##MIT License (MIT)
+
+The MIT License (MIT)
+
+Copyright (c) 2015-2016 geekpi
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
---
+++ spechtlite_conf.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# coding=utf-8
+
+import base64
+import requests
+import re
+import io
+
+
+def get_white_list():
+    print('start to generate whitelist')
+    whitelist_url = 'https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/accelerated-domains.china.conf'
+    data = requests.get(whitelist_url)
+    whitelist_path = './conf/whitelist'
+
+    with open(whitelist_path, 'w') as f:
+        for line in data.iter_lines():
+            domain = re.findall(r'\w+\.\w+', line.decode('utf-8'))
+            if len(domain) > 0:
+                f.write('%s\n' % '\.'.join(domain[0].split('.')))
+    print('whitelist done!')
+
+
+def get_gfw_list():
+    print('start to generate gfwlist')
+    gfwlist_url = 'https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt'
+    gfwlist_path = './conf/gfwlist'
+
+    comment_pattern = '^\!|\[|^@@|^\d+\.\d+\.\d+\.\d+'
+    domain_pattern = '([\w\-\_]+\.[\w\.\-\_]+)[\/\*]*'
+
+    data = requests.get(gfwlist_url).text
+    content = base64.b64decode(data)
+
+    with open(gfwlist_path, 'w') as gfwlist_path:
+        for line in io.StringIO(content.decode('utf-8')).readlines():
+            if re.findall(comment_pattern, line):
+                continue
+            else:
+                domain = re.findall(domain_pattern, line)
+                if domain:
+                    gfwlist_path.write('%s\n' % '\.'.join(domain[0].split('.')))
+    print('gfwlist done!')
+
+
+if __name__ == '__main__':
+    get_gfw_list()
+    get_white_list()
